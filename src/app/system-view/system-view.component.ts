e9173b5b7a3d66948aaeecbc65e107c6544a5126
julien yassef <juyassef@gmail.com>
2024-04-28 23:35:17 +0200

    first commit

---
+++ src/app/system-view/system-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-system-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './system-view.component.html',
+  styleUrl: './system-view.component.scss'
+})
+export class SystemViewComponent {
+
+}
