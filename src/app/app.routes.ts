e32ea2e2d49466bd52f6803ac007063739859196
julien yassef <juyassef@gmail.com>
2024-04-29 11:03:26 +0200

    création home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,10 @@ import { Routes } from '@angular/router';
 
 //Import Component
 import { PlanetsListComponent } from './planets-list/planets-list.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 
 export const routes: Routes = [
     { path: 'planets', component: PlanetsListComponent},
-    
-    
+    { path: '', component: HomePageComponent},
 ];
