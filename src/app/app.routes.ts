f2035fd5c329b1b067fef5dada778c79ab30db42
julien yassef <juyassef@gmail.com>
2024-05-06 01:35:04 +0200

    création page 404 error

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,12 @@ import { Routes } from '@angular/router';
 import { PlanetsListComponent } from './pages/planets-list/planets-list.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { PlanetDetailComponent } from './pages/planet-detail/planet-detail.component';
+import { Erreur404Component } from './erreur404/erreur404.component';
 
 
 export const routes: Routes = [
     { path: 'planets', component: PlanetsListComponent},
     { path: 'planets/:id', component: PlanetDetailComponent },
     { path: '', component: HomePageComponent},
+    { path: '**', component: Erreur404Component},
 ];
---
+++ src/app/erreur404/erreur404.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-erreur404',
+  standalone: true,
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './erreur404.component.html',
+  styleUrl: './erreur404.component.scss'
+})
+export class Erreur404Component {
+
+}
