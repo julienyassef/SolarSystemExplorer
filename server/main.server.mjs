ece6f276c0fd57a94803c0514c245995dddf5cc4
julien yassef <juyassef@gmail.com>
2024-05-31 22:13:36 +0200

    Auto-generated commit

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-CA6MPAJA.mjs";import"./chunk-DGQOGGGU.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-LZCUV5UI.mjs";import"./chunk-DGQOGGGU.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
